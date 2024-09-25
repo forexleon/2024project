3bad7e1b1535c2223cefcce53a13e16128e43a4e
forexleon <leon@tradingedge.net>
2024-09-25 19:46:30 +0800

    Add files via upload

--- Interface.py
+++ Interface.py
@@ -66,8 +66,8 @@ def RoundRobin_Insert(ratingstablename, userid, itemid, rating, openconnection):
     cur.execute("SELECT * FROM robbinsert")
     i = cur.fetchone()
     j = i[0]
-    cur.execute("INSERT INTO robin_part{0} VALUES({1},{2},{3})".format(j[0], userid, itemid, rating))
-    j = (j[0] + 1) % i[1]
+    cur.execute("INSERT INTO robin_part{0} VALUES({1},{2},{3})".format(j, userid, itemid, rating))
+    j = (j + 1) % i[1]
     cur.execute("UPDATE robbinsert SET partition_number={0}".format(j))
     con.commit()
 
